3795fbb6d14612550df4b8363d2ec8d8cb28dcfa
Fabien <lapertfabin@gmail.com>
2024-05-30 10:48:30 +0200

    feature(models): add client developer and project models

---
+++ database/migrations/2024_05_30_082840_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Client;
+use App\Models\Developer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->foreignIdFor(Client::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Developer::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
