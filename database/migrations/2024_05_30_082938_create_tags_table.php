1f5afb77dfd5c9d03fddf5403df5e0e3fde786b8
Fabien <lapertfabin@gmail.com>
2024-05-30 11:28:26 +0200

    feature(model): add tag model

---
+++ database/migrations/2024_05_30_082938_create_tags_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Tag;
+use App\Models\Task;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('isStatus');
+            $table->timestamps();
+        });
+
+
+        Schema::create('task-tags', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Task::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Tag::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+    }
+};
