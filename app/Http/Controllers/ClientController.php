83ce9a548b50f7f86df50ec7ec8860989b9e8674
Fabien <lapertfabin@gmail.com>
2024-05-30 11:48:58 +0200

    Refactor ClientController to use Client model and requests

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,66 +2,66 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreateAuteurRequest;
-use App\Models\Auteur;
+use App\Http\Requests\CreateClientRequest;
+use App\Models\Client;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 
-class AuteurController extends Controller
+class ClientController extends Controller
 {
     public function index(): View
     {
-        return view('auteur.index', [
-            'auteurs' => Auteur::orderBy('id', 'desc')->paginate(3)
+        return view('client.index', [
+            'clients' => Client::orderBy('id', 'desc')->paginate(3)
         ]);
     }
 
     public function show(string $name, string $firstname): View
     {
-        $auteur = Auteur::where('name', $name)->where('firstname', $firstname)->firstOrFail();
+        $client = Client::where('name', $name)->where('firstname', $firstname)->firstOrFail();
 
-        return view('auteur.show', [
-            'auteur' => $auteur
+        return view('client.show', [
+            'client' => $client
         ]);
     }
 
     public function create()
     {
-        $auteur = new Auteur();
-        return view('auteur.create', [
-            'auteur' => $auteur
+        $client = new Client();
+        return view('client.create', [
+            'client' => $client
         ]);
     }
 
-    public function store(CreateAuteurRequest $request)
+    public function store(CreateClientRequest $request)
     {
-        $auteur = Auteur::create($request->validated());
+        $client = Client::create($request->validated());
 
-        return redirect()->route('auteur.show', [
-            'name' => $auteur->name,
-            'firstname' => $auteur->firstname
-        ])->with('success', "L'auteur a bien été sauvegardé");
+        return redirect()->route('client.show', [
+            'name' => $client->name,
+            'firstname' => $client->firstname
+        ])->with('success', "L'client a bien été sauvegardé");
     }
 
 
     public function edit(string $name, string $firstname): View
     {
-        $auteur = Auteur::where('name', $name)->where('firstname', $firstname)->firstOrFail();
+        $client = Client::where('name', $name)->where('firstname', $firstname)->firstOrFail();
 
-        return view('auteur.edit', [
-            'auteur' => $auteur
+        return view('client.edit', [
+            'client' => $client
         ]);
     }
 
-    public function update(CreateAuteurRequest $request, string $name, string $firstname): RedirectResponse
+    public function update(CreateClientRequest $request, string $name, string $firstname): RedirectResponse
     {
-        $auteur = Auteur::where('name', $name)->where('firstname', $firstname)->firstOrFail();
-        $auteur->update($request->validated());
+        $client = Client::where('name', $name)->where('firstname', $firstname)->firstOrFail();
+        $client->update($request->validated());
 
-        return redirect()->route('auteur.show', [
-            'name' => $auteur->name,
-            'firstname' => $auteur->firstname
-        ])->with('success', "L'auteur a bien été mis à jour");
+        return redirect()->route('client.show', [
+            'name' => $client->name,
+            'firstname' => $client->firstname
+        ])->with('success', "L'client a bien été mis à jour");
     }
 }
